613c8f8b83a44339dac5cecd45f45dffe4a2f758
Станислав Маяцкий <stasm226@gmail.com>
2025-05-03 20:40:50 +0500

    feat: add settings for sqligt2 db for testings

--- config/settings.py
+++ config/settings.py
@@ -206,7 +206,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = os.getenv("EMAIL_PORT", 587)
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True")
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# Настройка лёгкой БД для тестов
+if "test" in sys.argv:
+    DATABASES["default"] = {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
