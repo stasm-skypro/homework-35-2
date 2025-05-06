06697eaf604c44956d6b7018dce68117b18e0a44
Станислав Маяцкий <stasm226@gmail.com>
2025-05-06 11:16:53 +0500

    ref: move the creation log files from project Dockerfile into the entrypoint_*.sh files

--- entrypoint_celery.sh
+++ entrypoint_celery.sh
@@ -12,5 +12,9 @@ echo "PostgreSQL started"
 # Применяем миграции
 python manage.py migrate
 
+# Создаём директории логов и файлы логов
+mkdir -p /app/materials/logs && mkdir -p /app/users/logs
+touch /app/materials/logs/reports.log && touch /app/users/logs/reports.log
+
 # Запускаем Celery
 celery -A config worker --loglevel=info
